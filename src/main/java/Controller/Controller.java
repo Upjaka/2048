3df300d0a92dc8c2e0093b8e73dbdea13e32d26f
Upjaka <alesant-l-99@mail.ru>
2021-04-16 17:57:47 +0300

    начало FXML

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,8 +1,34 @@
 package Controller;
 
 import Model.Field;
+import View.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class Controller {
-    private final static Field field = new Field();
+    private Field field;
 
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button recordsButton;
+    @FXML
+    private Button restartButton;
+
+    public Controller() {}
+
+    public void startButtonClicked() throws IOException {
+        System.out.println("Start!");
+        field = new Field();
+        Main.startGame();
+    }
+
+    public void restartButtonClicked() {
+        System.out.println("Restart!");
+        field = new Field();
+        Main.restartGame();
+    }
 }
--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -1,21 +1,47 @@
 package View;
 
+import Controller.Controller;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Group root = new Group();
-        Scene scene = new Scene(root, 800, 600);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("2048");
-        primaryStage.show();
-    }
+    public static Stage stage;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL startScreenUrl = new File("files/StartScreen.fxml").toURI().toURL();
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = FXMLLoader.load(startScreenUrl);
+
+        Scene startScene = new Scene(root, 400, 600);
+        stage = primaryStage;
+        stage.setScene(startScene);
+        stage.setTitle("2048");
+        stage.show();
+    }
+
+    public static void startGame() throws IOException {
+        URL GameScreenUrl = new File("files/GameScreen.fxml").toURI().toURL();
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = FXMLLoader.load(GameScreenUrl);
+        Scene gameScene = new Scene(root, 400, 600);
+        stage.setScene(gameScene);
+        stage.setTitle("2048");
+        stage.show();
+    }
+
+    public static void restartGame() {
+
     }
 }
